455dd00a62cf2e56ffb9a4cb3f6e6b3129767a08
Daniil <daniilrealgrad@gmail.com>
2020-03-21 11:07:59 +0300

    Updates

---
+++ precache-manifest.68027ea4f6f00a203e3f8fdf2573c8f6.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9558b0274ad61c94ebdaee564fff0838",
+    "url": "/repository-name/index.html"
+  },
+  {
+    "revision": "4042eed25003de780210",
+    "url": "/repository-name/static/css/4.98099b0d.chunk.css"
+  },
+  {
+    "revision": "804aeef7ac80cb719d8d",
+    "url": "/repository-name/static/css/5.ed38649d.chunk.css"
+  },
+  {
+    "revision": "db799784468e01620f59",
+    "url": "/repository-name/static/css/6.00cf3a7b.chunk.css"
+  },
+  {
+    "revision": "d29c50a4c5a0c1a9efc5",
+    "url": "/repository-name/static/css/main.8f424003.chunk.css"
+  },
+  {
+    "revision": "d114a610ca289b26ea8e",
+    "url": "/repository-name/static/js/0.47411739.chunk.js"
+  },
+  {
+    "revision": "86cd2a75214d8a2ef9ef",
+    "url": "/repository-name/static/js/3.a83ceb80.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/repository-name/static/js/3.a83ceb80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4042eed25003de780210",
+    "url": "/repository-name/static/js/4.dee651cc.chunk.js"
+  },
+  {
+    "revision": "804aeef7ac80cb719d8d",
+    "url": "/repository-name/static/js/5.77622926.chunk.js"
+  },
+  {
+    "revision": "db799784468e01620f59",
+    "url": "/repository-name/static/js/6.fec16bf1.chunk.js"
+  },
+  {
+    "revision": "f65b4f485f197c2e477a",
+    "url": "/repository-name/static/js/7.b1ec352a.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/repository-name/static/js/7.b1ec352a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d29c50a4c5a0c1a9efc5",
+    "url": "/repository-name/static/js/main.0b4dc4cf.chunk.js"
+  },
+  {
+    "revision": "b59d09a37558c39e1a77",
+    "url": "/repository-name/static/js/runtime-main.3d15a9fa.js"
+  }
+]);
